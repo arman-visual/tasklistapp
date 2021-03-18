9a9f9d5d435bbc8ce689cece90c4455e0411cddf
Arman <arman.agora@gmail.com>
2021-03-18 22:09:02 +0100

    first commit

---
+++ app/src/main/java/com/avisual/tasklistapp/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.avisual.tasklistapp
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/avisual/tasklistapp/model/Task.kt
@@ -0,0 +1,3 @@
+package com.avisual.tasklistapp.model
+
+data class Task(val title: String, val description: String)
