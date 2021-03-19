88f0ce76db7fd15bf20efd021f8d7746bb55af72
Arman <arman.agora@gmail.com>
2021-03-19 12:05:44 +0100

    Added RegisterActivity and ViewModel also MainViewModel
    Añadido Activities y sus respectivos viewModels
    añadido XML de registro de tareas

---
+++ app/src/main/java/com/avisual/tasklistapp/ui/RegisterTaskActivity.kt
@@ -0,0 +1,44 @@
+package com.avisual.tasklistapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.avisual.tasklistapp.database.Db
+import com.avisual.tasklistapp.databinding.ActivityRegisterTaskBinding
+import com.avisual.tasklistapp.model.Task
+import com.avisual.tasklistapp.repository.TaskRepository
+
+class RegisterTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterTaskBinding
+    private lateinit var taskRepository: TaskRepository
+    private lateinit var viewModel: RegisterViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        buildDependencies()
+        viewModel = buildViewModel()
+        binding = ActivityRegisterTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonSave.setOnClickListener { saveTask() }
+    }
+
+    private fun buildDependencies() {
+        val database = Db.getDatabase(this.applicationContext)
+        taskRepository = TaskRepository(database)
+    }
+
+    private fun buildViewModel(): RegisterViewModel {
+        val factory = RegisterViewModelFactory(taskRepository)
+        return ViewModelProvider(this, factory).get(RegisterViewModel::class.java)
+    }
+
+    private fun saveTask() {
+        val title = binding.etTitle.text.toString()
+        val description = binding.etDescription.text.toString()
+        val task = Task(0, title, description)
+        viewModel.saveTask(task)
+        finish()
+    }
+
+}
---
+++ app/src/main/java/com/avisual/tasklistapp/ui/RegisterViewModel.kt
@@ -0,0 +1,25 @@
+package com.avisual.tasklistapp.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.avisual.tasklistapp.common.ScopeViewModel
+import com.avisual.tasklistapp.model.Task
+import com.avisual.tasklistapp.repository.TaskRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class RegisterViewModel(private val taskRepository: TaskRepository) : ScopeViewModel() {
+
+    fun saveTask(task: Task) {
+        launch(Dispatchers.IO) {
+            taskRepository.save(task)
+        }
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class RegisterViewModelFactory(private val taskRepository: TaskRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        RegisterViewModel(taskRepository) as T
+}
