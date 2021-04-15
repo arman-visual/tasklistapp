2e6c40430d9644d0a5337a7d8aadc16d020aa97b
ArmanMac <Arman.agora@gmail.com>
2021-04-16 01:36:33 +0200

    Added in VM for listen a flow that after it'll convert in livedata

--- app/src/main/java/com/avisual/tasklistapp/ui/MainViewModel.kt
+++ app/src/main/java/com/avisual/tasklistapp/ui/MainViewModel.kt
@@ -1,30 +1,17 @@
 package com.avisual.tasklistapp.ui
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import com.avisual.tasklistapp.common.ScopeViewModel
 import com.avisual.tasklistapp.model.Task
 import com.avisual.tasklistapp.repository.TaskRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainViewModel(private val taskRepository: TaskRepository) : ScopeViewModel() {
 
-    private val _storedTask = MutableLiveData<List<Task>>()
-    val storedTask: LiveData<List<Task>> get() = _storedTask
-    //val storedTask: LiveData<List<Task>> get() = taskRepository.getAllLiveData()
+    val storedTask: LiveData<List<Task>> get() = taskRepository.getAllTasks().asLiveData()
 
-    init {
-        refresh()
-    }
-
-    fun refresh() {
-        launch(Dispatchers.IO) {
-            _storedTask.postValue(taskRepository.getAll())
-        }
-    }
 }
 
 @Suppress("UNCHECKED_CAST")
