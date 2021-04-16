966c77a6f6448a9802a1f719ebddd96e9731f952
Arman <arman.agora@gmail.com>
2021-04-16 19:56:39 +0200

    Added file for stored functions extensions

---
+++ app/src/main/java/com/avisual/tasklistapp/common/Extensions.kt
@@ -0,0 +1,24 @@
+package com.avisual.tasklistapp.common
+
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+inline fun <reified T : ViewModel> FragmentActivity.getViewModel(crossinline factory: () -> T): T {
+
+    val vmFactory = object : ViewModelProvider.Factory {
+        override fun <U : ViewModel> create(modelClass: Class<U>): U = factory() as U
+    }
+
+    return ViewModelProvider(this, vmFactory).get()
+}
+
+fun <T> CoroutineScope.collectFlow(flow: Flow<T>, body: suspend (T) -> Unit) {
+    flow.onEach { body(it) }
+        .launchIn(this)
+}
